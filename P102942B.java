00776d7eaf0afa0b8faab754117834997f2fdfd6
Bennet Otoho <bennet2000@outlook.com>
2021-07-03 22:43:47 +0100

    Add P102942B.java

---
+++ P102942B.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P102942B {
+  public static void main(String[] args) {
+    SpeedScanner in = new SpeedScanner();
+    int t = in.nextInt();
+    while(t-- != 0) {
+      int n = in.nextInt(), evens = 0;
+      for(int i = 0; i < n; ++i)
+        if((in.nextInt() & 1) == 0) ++evens;
+      System.out.println(evens == n ? -1 : evens);
+    }
+  }
+
+  private static class SpeedScanner {
+    BufferedReader in;
+    StringTokenizer splitter;
+  
+    public SpeedScanner() {
+      in = new BufferedReader(new InputStreamReader(System.in));
+    }
+  
+    String next() {
+      while(splitter == null || !splitter.hasMoreElements())
+        try {
+          splitter = new StringTokenizer(in.readLine());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      return splitter.nextToken();
+    }
+  
+    String nextLine() {
+      String s = "";
+      try {
+        s = in.readLine();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      return s;
+    }
+  
+    int nextInt() {
+      return Integer.parseInt(next());
+    }
+  
+    long nextLong() {
+      return Long.parseLong(next());
+    }
+  
+    double nextDouble() {
+      return Double.parseDouble(next());
+    }
+  }
+}
