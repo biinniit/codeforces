0f12d4a4a924087732b779101771f0a975f96805
Bennet Otoho <bennet2000@outlook.com>
2024-06-03 15:55:11 +0100

    Add P1980A.java

---
+++ P1980A.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.*;
+
+public class P1980A {
+  public static void main(String[] args) {
+    SpeedScanner in = new SpeedScanner();
+    int t = in.nextInt();
+
+    while (t-- != 0) {
+      int n = in.nextInt(), m = in.nextInt(), ans = m * 7;
+      int[] bank_i = {0, 0, 0, 0, 0, 0, 0};
+      char[] a = in.nextLine().toCharArray();
+
+      for (char i: a)
+        if (++bank_i[i - 'A'] <= m) --ans;
+
+      System.out.println(ans);
+    }
+  }
+
+  static class SpeedScanner {
+    BufferedReader in;
+    StringTokenizer splitter;
+  
+    public SpeedScanner() {
+      in = new BufferedReader(new InputStreamReader(System.in));
+    }
+  
+    String next() {
+      while (splitter == null || !splitter.hasMoreElements())
+        try {
+          splitter = new StringTokenizer(in.readLine());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      return splitter.nextToken();
+    }
+  
+    String nextLine() {
+      String s = "";
+      try {
+        s = in.readLine();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      return s;
+    }
+  
+    int nextInt() {
+      return Integer.parseInt(next());
+    }
+  
+    long nextLong() {
+      return Long.parseLong(next());
+    }
+  
+    double nextDouble() {
+      return Double.parseDouble(next());
+    }
+  }
+}
