d567f9630ee77f1260acb352e27dd8076c1b975b
Bennet Otoho <bennet2000@outlook.com>
2025-03-01 12:02:06 +0100

    configure editor settings

--- p2070a.js
+++ p2070a.js
@@ -1,6 +1,6 @@
 const { createInterface } = require('readline')
 
-const rl = createInterface({input: process.stdin, output: process.stdout, prompt: '' })
+const rl = createInterface({ input: process.stdin, output: process.stdout, prompt: '' })
 const input = []
 
 rl.prompt()
@@ -10,8 +10,8 @@ rl.on('line', (line) => {
 rl.on('close', () => {
   let t = parseInt(input[0])
 
-  while(t-- !== 0) {
+  while (t-- !== 0) {
     const n = parseInt(input[input.length - t - 1])
-    console.log(Math.floor(n / 15) * 3 + (n % 15 < 2 ? n % 15 + 1 : 3))
+    console.log(Math.floor(n / 15) * 3 + (n % 15 < 2 ? (n % 15) + 1 : 3))
   }
 })
